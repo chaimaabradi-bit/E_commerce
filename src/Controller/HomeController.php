cff89937731202c1d8571f84ba140987e3ce1c79
Chaima <chaima.abradi@pneus-online.com>
2025-08-27 17:48:17 +0200

    Correction HomeController + ajout config monolog + nouvelles migrations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,21 +5,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Product;
+use Psr\Log\LoggerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(HttpClientInterface $client): Response
+    public function index(HttpClientInterface $client,EntityManagerInterface $em,LoggerInterface $logger): Response
     {
         //Appel à l'api https://fakestoreapi.com/products
         $response = $client->request('GET', 'https://dummyjson.com/products');
-
-        //Faire un try catch 
+         $logger->info('Test de log Symfony en mode dev');
         try{
             $arrayResponse = json_decode($response->getContent());
-            dump($arrayResponse->products);
-        } catch (Exception $e) {
-            echo 'Exception: ',  $e->getMessage(), "\n";
+            //Insertion en base de donnée
+            foreach($arrayResponse->products as $objProduct) {
+                $product = new Product();
+                $product->setTitle($objProduct->title);
+                $product->setPrice($objProduct->price);
+                $product->setDescription($objProduct->description);
+                $product->setCategory($objProduct->category);
+                $product->setImage($objProduct->images[0] ?? null);
+                $em->persist($product);
+            }
+            $em->flush();
+       } catch (\Exception $e) {
+        echo "Erreur : " . $e->getMessage();
         }
 
         return $this->render('home/index.html.twig', [
